b261aa6e2c89341a92f9b75bc91c3378672c7aa9
Hudson <Simon.Hudson@experian.com>
2020-04-21 08:49:49 +0100

    css

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '&/css/main.scss';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function App({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
