55c19a6477bf0818393e2af1b4893c04cbc29cc6
Hudson <Simon.Hudson@experian.com>
2020-05-01 15:21:43 +0100

    fix hero

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -5,22 +5,26 @@ import { Wrapper, Title } from './index.styles';
 
 const CONFIG = {
 	home: {
-		title: 'Welcome to our site',
+		title: 'Welcome to our site.',
 		useLargeHero: true,
 	},
 	about: {
-		title: 'About us',
+		title: 'About us.',
 	},
 	contact: {
-		title: 'Get in touch',
+		title: 'Get in touch.',
+	},
+	_error: {
+		title: 'Sorry, something has gone wrong.',
 	},
 };
 
 const Hero = (props) => {
+	const title = CONFIG[props.currentPage.name] ? CONFIG[props.currentPage.name].title : '';
 	return (
 		<Wrapper data-test="hero">
 			<props.theme.layout.Wrap>
-				<Title data-test="hero__title">{CONFIG[props.currentPage.name].title}</Title>
+				<Title data-test="hero__title">{title}</Title>
 			</props.theme.layout.Wrap>
 		</Wrapper>
 	);
