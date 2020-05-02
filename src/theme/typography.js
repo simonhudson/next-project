e36d4153713fa1fdae997868c976b519df461e77
Hudson <Simon.Hudson@experian.com>
2020-05-02 14:56:29 +0100

    typography

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -1,19 +1,34 @@
 'use strict';
 
+import React from 'react';
 import styled from 'styled-components';
 import { rem } from 'polished';
 
-const H1 = styled.h1`
-	color: ${({ theme }) => theme.palette.primary.b};
-	font-weight: 500;
-	font-size: ${rem(28)};
-`;
+const isValidHeading = (element) => element && ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(element.toLowerCase());
 
-const H2 = styled.h2`
-	color: ${({ theme }) => theme.palette.primary.b};
-	font-family: 'Lora', georgia, 'times new roman', serif;
-	font-weight: 400;
-	font-size: ${rem(28)};
-`;
+const H1 = (props) => {
+	const element = isValidHeading(props.element) ? props.element.toLowerCase() : 'h1';
+
+	const StyledH1 = styled[element]`
+		color: ${({ theme }) => theme.palette.primary.b};
+		font-family: 'Lora', sans-serif;
+		font-weight: 400;
+		font-size: ${rem(28)};
+	`;
+
+	return <StyledH1>{props.children}</StyledH1>;
+};
+
+const H2 = (props) => {
+	const element = isValidHeading(props.element) ? props.element.toLowerCase() : 'h2';
+
+	const StyledH2 = styled[element]`
+		color: ${({ theme }) => theme.palette.primary.bodyText};
+		font-weight: 500;
+		font-size: ${rem(18)};
+	`;
+
+	return <StyledH2>{props.children}</StyledH2>;
+};
 
 export { H1, H2 };
