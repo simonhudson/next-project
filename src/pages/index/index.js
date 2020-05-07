7d2b50a7f2d233659836c74f26442eda1526ffec
Hudson <Simon.Hudson@experian.com>
2020-05-07 09:38:30 +0100

    page heading

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,7 +7,7 @@ import { layout, typography } from '~/theme';
 const Home = (props) => {
 	return (
 		<layout.Wrap>
-			<typography.H1>Heading 1</typography.H1>
+			<typography.H1>Develop branch</typography.H1>
 			<p>This page has been rendered on the server. You can turn JavaScript off and it will still be visible</p>
 			<p>The content below has been rendered on the client</p>
 			<DataList endpoint="users" />
