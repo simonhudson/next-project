2e7ac4db7bbb6b143213cb0d6ace8a998001aa56
Hudson <Simon.Hudson@experian.com>
2020-06-04 09:37:20 +0100

    update plugin

--- plugins/netlify-plugin-lighthouse/index.js
+++ plugins/netlify-plugin-lighthouse/index.js
@@ -6,7 +6,7 @@ const URL = 'https://next.simonhudson.net';
 module.exports = {
 	onPostBuild: () => {
 		console.log(`Running Lighthouse on ${URL}`);
-		lighthouse(URL).then((results) => {
+		return lighthouse(URL).then((results) => {
 			const resultsJson = results.report;
 			console.log('---------------');
 			console.log(JSON.stringify(resultsJson));
