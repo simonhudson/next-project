38a2b96d38d0602026a35b5ffd7b580d980a778b
Hudson <Simon.Hudson@experian.com>
2020-06-04 09:21:22 +0100

    plugins

---
+++ plugins/netlify-plugin-lighthouse/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const lighthouse = require('lighthouse');
+const URL = 'https://next.simonhudson.net';
+
+module.exports = {
+	onPostBuild: () => {
+		console.log(`Running Lighthouse on ${URL}`);
+		lighthouse(URL).then((results) => {
+			const resultsJson = results.report;
+			console.log('---------------');
+			console.log(JSON.stringify(resultsJson));
+			console.log('---------------');
+		});
+	},
+};
