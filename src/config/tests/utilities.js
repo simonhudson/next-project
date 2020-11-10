fface6b6be3eb211f3a681b6e0bd39c385d7e6ba
Hudson <Simon.Hudson@experian.com>
2020-11-10 09:16:51 +0000

    test utils

--- src/config/tests/utilities.js
+++ src/config/tests/utilities.js
@@ -6,29 +6,19 @@ import { mount, shallow } from 'enzyme';
 import { ThemeProvider } from 'styled-components';
 import Theme from '~/theme';
 
-const assertElementExists = (objectUnderTest, elements) => {
+const assertElementState = (objectUnderTest, elements, expectedState) => {
 	const doAssertion = (element) => {
-		expect(objectUnderTest.exists(element)).to.be.true;
-		expect(objectUnderTest.find(element).length).to.equal(1);
+		expect(objectUnderTest.exists(element)).to.be[expectedState];
+		expect(objectUnderTest.find(element).length).to.equal(!!expectedState ? 1 : 0);
 	};
 
 	if (Array.isArray(elements)) elements.forEach((element) => doAssertion(element));
 	else doAssertion(elements);
 };
 
-const assertElementDoesNotExist = (objectUnderTest, elements) => {
-	const doAssertion = (element) => {
-		expect(objectUnderTest.exists(element)).to.be.false;
-		expect(objectUnderTest.find(element).length).to.equal(0);
-	};
-
-	if (Array.isArray(elements)) elements.forEach((element) => doAssertion(element));
-	else doAssertion(elements);
-};
-
-const mountWithTheme = (childToMount) => {
-	return mount(shallow(<ThemeProvider theme={Theme}>{childToMount}</ThemeProvider>).get(0));
-};
+const assertElementExists = (objectUnderTest, elements) => assertElementState(objectUnderTest, elements, true);
+const assertElementDoesNotExist = (objectUnderTest, elements) => assertElementState(objectUnderTest, elements, false);
+const mountWithTheme = (childToMount) => mount(shallow(<ThemeProvider theme={Theme}>{childToMount}</ThemeProvider>).get(0));
 
 module.exports = {
 	assertElementExists,
